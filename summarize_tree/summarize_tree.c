3195d590a9c3afea87213dfe90abf8483c521fd5
JoshuaHamann <haman127@morris.umn.edu>
2019-10-08 15:21:01 -0500

    implements is_dir

--- summarize_tree/summarize_tree.c
+++ summarize_tree/summarize_tree.c
@@ -16,6 +16,11 @@ bool is_dir(const char* path) {
    * return value from stat in case there is a problem, e.g., maybe the
    * the file doesn't actually exist.
    */
+   struct stat buf;
+   if( stat(path,&buf) == 0 ){
+	   return S_ISDIR(buf.st_mode)
+   } else return false;
+
 }
 
 /* 
